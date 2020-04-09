d7345d19ecfcbd8f79e838b527f5d5d7f8d7c364
Erick Pérez <erick.leao.perez@gmail.com>
2020-04-08 18:08:49 -0600

    Manejando nuestras rutas y renderizando el contenido en nuestras páginas

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,17 +1,25 @@
-const Character = () => {
+import getData from '../utils/getData'
+import getHash from '../utils/getHash'
+
+const Character = async () => {
+    const id = getHash()
+    const character = await getData(id)
+
+    const { name, image, episode, status, species, gender, origin, location } = character
+
     const view = `
         <div class="Character-inner">
             <article class="Characters-card">
-                <img src="image" alt="name">
-                <h2>Name</h2>
+                <img src="${image}" alt="${name}">
+                <h2>${name}</h2>
             </article>
             <article class="Characters-card">
-                <h3>Episodes:</h3>
-                <h3>Status:</h3>
-                <h3>Species:</h3>
-                <h3>Gender:</h3>
-                <h3>Origin:</h3>
-                <h3>Last location:</h3>
+                <h3>Episodes: <span>${episode.length}</span></h3>
+                <h3>Status: <span>${status}</span></h3>
+                <h3>Species: <span>${species}</span></h3>
+                <h3>Gender: <span>${gender}</span></h3>
+                <h3>Origin: <span>${origin.name}</span></h3>
+                <h3>Last location: <span>${location.name}</span></h3>
             </article>
         </div>
     `
